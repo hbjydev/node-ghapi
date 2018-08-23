429500ebbfc4a5c806c441e80cff643584d04086
Hayden Young <hayden@haydenbjyoung.me>
2018-08-23 20:55:23 +0100

    Added the client class file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-module.exports = {
-  users: require('./user'),
-  repos: require('./repo')
+const 
+
+/**
+ * The client to connect to the API with
+ * @class
+ */
+class GitHubAPIClient {
+  constructor(options) {
+    this.token = options.token || null;
+  }
+
+  users (username) {
+    
+  }
 }
+
+module.exports = GitHubAPIClient;
