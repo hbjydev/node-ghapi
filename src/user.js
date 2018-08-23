d6741ae3f24d9dfe23ab3b5c9ed227b8927478c4
Hayden Young <hayden@haydenbjyoung.me>
2018-08-23 02:05:37 +0100

    Changed how functions are stored and rebuild docs

--- src/user.js
+++ src/user.js
@@ -1,29 +1,28 @@
 const fetch = require('node-fetch');
 
-module.exports = {
-  /**
-   * Gets a User by username. Must be run asynchronously.
-   * @function
-   * @param {string} username The username of the user to find. (`login` parameter on the API)
-   * @example
-   * await ghapi.users.get('haydennyyy');
-   */
-  get: username => {
-    return fetch('https://api.github.com/users/' + username, {
-      headers: { 'User-Agent': 'node-ghapi' }
-    }).then(user => user.json())
-  },
-
-  /**
-   * Gets a user's repositories by username. Must be run asynchronously.
-   * @function
-   * @param {string} username The username of the user to find repos for. (`login` parameter on the API)
-   * @example
-   * await ghapi.users.getRepos('haydennyyy');
-   */
-  getRepos: username => {
-    return fetch(`https://api.github.com/users/${username}/repos`, {
-      headers: { 'User-Agent': 'node-ghapi' }
-    }).then(repos => repos.json())
-  }
+/**
+ * Gets a User by username. Must be run asynchronously.
+ * @function
+ * @param {string} username The username of the user to find. (`login` parameter on the API)
+ * @example
+ * await ghapi.users.get('haydennyyy');
+ */
+function get (username) {
+  return fetch('https://api.github.com/users/' + username, {
+    headers: { 'User-Agent': 'node-ghapi' }
+  }).then(user => user.json())
 }
+
+/**
+ * Gets a user's repositories by username. Must be run asynchronously.
+ * @function
+ * @param {string} username The username of the user to find repos for. (`login` parameter on the API)
+ * @example
+ * await ghapi.users.getRepos('haydennyyy');
+ */
+function getRepos (username) {
+  return fetch(`https://api.github.com/users/${username}/repos`, {
+    headers: { 'User-Agent': 'node-ghapi' }
+  }).then(repos => repos.json())
+}
+module.exports = { get, getRepos }
